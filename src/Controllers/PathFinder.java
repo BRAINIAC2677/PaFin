1ea969ad9ff05710a30bb737a70b15dd29bb2d43
Asif Azad <66885542+BRAINIAC2677@users.noreply.github.com>
2021-04-16 11:02:46 +0600

    Basic UI Done

--- src/Controllers/PathFinder.java
+++ src/Controllers/PathFinder.java
@@ -1,5 +1,50 @@
 package Controllers;
 
 
-public class PathFinder {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class PathFinder implements Initializable {
+    @FXML
+    private FlowPane centerGrid;
+    @FXML
+    ComboBox<String> nodeComboBox;
+    @FXML
+    ComboBox<String> speedComboBox;
+    @FXML
+    ComboBox<String> algoComboBox;
+    @FXML
+    ComboBox<String> themeComboBox;
+    private int gridRowNo = 20, gridColNo = 38;
+    private int cellWidth = 25, cellHeight = 25;
+    private Rectangle[][] gridCells = new Rectangle[gridRowNo][gridColNo];
+    private String[] nodeItems = {"Source", "Destination", "Wall"};
+    private String[] speedItems = {"Fast", "Medium", "Slow"};
+    private String[] algoItems = {"Depth-First-Search", "Breadth-First-Search"};
+    private String[] themeItems = {"Light", "Dark"};
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        for(int i = 0;i < gridRowNo; i++){
+            for(int j = 0;j < gridColNo; j++){
+                gridCells[i][j] = new Rectangle();
+                gridCells[i][j].setWidth(cellWidth);
+                gridCells[i][j].setHeight(cellHeight);
+                gridCells[i][j].getStyleClass().add("grid-cell");
+                centerGrid.getChildren().add(gridCells[i][j]);
+            }
+        }
+        nodeComboBox.getItems().addAll(nodeItems);
+        speedComboBox.getItems().addAll(speedItems);
+        algoComboBox.getItems().addAll(algoItems);
+        themeComboBox.getItems().addAll(themeItems);
+    }
 }
+
